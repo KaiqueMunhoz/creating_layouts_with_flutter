3bf71e7b27143e99afba0925e0b60454baf02089
Kaique Munhoz <munhoz13km@gmail.com>
2020-03-18 18:28:55 -0300

    Feat: Using themes and colors

--- flutter/02-module/building_basic_layout/lib/main.dart
+++ flutter/02-module/building_basic_layout/lib/main.dart
@@ -8,6 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Color(0xff009688),
+        accentColor: Color(0xff7C4DFF),
+        textTheme: TextTheme(
+          body1: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(title: Text('Building Layouts with Flutter')),
         body: Center(
