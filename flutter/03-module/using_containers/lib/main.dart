e36fbf3dbd56e751863595831ebcd709ed47cb31
Kaique Munhoz <munhoz13km@gmail.com>
2020-03-18 19:04:57 -0300

    Feat: Using gradients

--- flutter/03-module/using_containers/lib/main.dart
+++ flutter/03-module/using_containers/lib/main.dart
@@ -14,40 +14,97 @@ class MyApp extends StatelessWidget {
           body: Column(
             children: <Widget>[
               Container(
-                margin: EdgeInsets.all(100.0),
-                color: Colors.orange,
-                width: 100.0,
-                height: 100.0,
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 100.0),
-                color: Colors.blue,
-                width: 100.0,
-                height: 100.0,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 16.0),
-                color: Colors.purple,
-                width: 100.0,
-                height: 100.0,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 16.0),
-                width: 100.0,
-                height: 100.0,
-                decoration:
-                    BoxDecoration(color: Colors.red, shape: BoxShape.circle),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 16.0),
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
                 width: 100.0,
                 height: 100.0,
                 decoration: BoxDecoration(
-                  color: Colors.green,
+                  color: Colors.red,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(25),
+                  ),
+                  gradient: LinearGradient(
+                      colors: [Colors.purple[50], Colors.purple[500]]),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.green[50], Colors.green[500]]),
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(25.0),
                     bottomRight: Radius.circular(10.0),
                   ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment(0.0, -1.0),
+                      end: Alignment(0.0, -0.8),
+                      colors: [Colors.blue[50], Colors.blue[500]]),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment(0.0, -1.0),
+                      end: Alignment(0.0, -0.8),
+                      tileMode: TileMode.repeated,
+                      colors: [Colors.red[50], Colors.red[500]]),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment(0.0, -1.0),
+                      end: Alignment(0.0, -0.8),
+                      tileMode: TileMode.mirror,
+                      colors: [Colors.red[50], Colors.red[500]]),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Colors.purple,
+                    Colors.green,
+                    Colors.blue,
+                    Colors.red,
+                  ]),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(stops: [
+                    0.5,
+                    0.6,
+                    0.7,
+                    0.8
+                  ], colors: [
+                    Colors.purple,
+                    Colors.green,
+                    Colors.blue,
+                    Colors.red,
+                  ]),
                 ),
               ),
             ],
