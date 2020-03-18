717eb31beb556e1f6412731b98572d78b277f27e
Kaique Munhoz <munhoz13km@gmail.com>
2020-03-18 19:25:41 -0300

    Feat: Radius

--- flutter/03-module/using_containers/lib/main.dart
+++ flutter/03-module/using_containers/lib/main.dart
@@ -18,70 +18,7 @@ class MyApp extends StatelessWidget {
                 width: 100.0,
                 height: 100.0,
                 decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25),
-                  ),
-                  gradient: LinearGradient(
-                      colors: [Colors.purple[50], Colors.purple[500]]),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 100.0, top: 10.0),
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.green[50], Colors.green[500]]),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25.0),
-                    bottomRight: Radius.circular(10.0),
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 100.0, top: 10.0),
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment(0.0, -1.0),
-                      end: Alignment(0.0, -0.8),
-                      colors: [Colors.blue[50], Colors.blue[500]]),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 100.0, top: 10.0),
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment(0.0, -1.0),
-                      end: Alignment(0.0, -0.8),
-                      tileMode: TileMode.repeated,
-                      colors: [Colors.red[50], Colors.red[500]]),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 100.0, top: 10.0),
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment(0.0, -1.0),
-                      end: Alignment(0.0, -0.8),
-                      tileMode: TileMode.mirror,
-                      colors: [Colors.red[50], Colors.red[500]]),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(left: 100.0, top: 10.0),
-                width: 100.0,
-                height: 100.0,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
+                  gradient: RadialGradient(colors: [
                     Colors.purple,
                     Colors.green,
                     Colors.blue,
@@ -94,18 +31,46 @@ class MyApp extends StatelessWidget {
                 width: 100.0,
                 height: 100.0,
                 decoration: BoxDecoration(
-                  gradient: LinearGradient(stops: [
-                    0.5,
-                    0.6,
-                    0.7,
-                    0.8
-                  ], colors: [
+                  gradient: RadialGradient(radius: 0.25, colors: [
                     Colors.purple,
                     Colors.green,
                     Colors.blue,
                     Colors.red,
                   ]),
                 ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 150.0,
+                decoration: BoxDecoration(
+                    gradient: RadialGradient(
+                        radius: 0.25,
+                        center: Alignment(0.0, 0.5),
+                        colors: [
+                          Colors.purple,
+                          Colors.green,
+                          Colors.blue,
+                          Colors.red,
+                        ]),
+                    borderRadius: BorderRadius.all(Radius.circular((25)))),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 100.0, top: 10.0),
+                width: 100.0,
+                height: 150.0,
+                child: FlutterLogo(),
+                decoration: BoxDecoration(
+                    gradient: RadialGradient(
+                        radius: 0.25,
+                        center: Alignment(0.0, 0.5),
+                        colors: [
+                          Colors.purple,
+                          Colors.green,
+                          Colors.blue,
+                          Colors.red,
+                        ]),
+                    borderRadius: BorderRadius.all(Radius.circular((25)))),
               ),
             ],
           )),
